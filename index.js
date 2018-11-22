db98fd805f2b6610f91e62673aaf6e21c28e21c6
Nicholas Gao <nicholas.gao96@gmail.com>
2018-11-22 19:07:29 +0000

    updated server implementation

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const WebSocket = require('ws');
 
 const app = express();
-const wss = new WebSocket.Server({ port: 8080 });
+const port = 3001
+const wss = new WebSocket.Server({
+    port: 8080
+});
 
 const request = require('request');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-const randomstring = require("randomstring");
 
 // Connection URL
 const url = "mongodb+srv://SenneS_Admin:SenneS2018@sennescluster-onimy.mongodb.net/SenneSDB";
@@ -20,17 +22,17 @@ var dbClient;
 
 // We will also provide a websocket for better communication
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-      messageHandling(message, {
-          send : obj => ws.send(JSON.stringify(obj))
-      });
-  });
+    ws.on('message', function incoming(message) {
+        messageHandling(message, {
+            send: obj => ws.send(JSON.stringify(obj))
+        });
+    });
 
-  ws.send('something');
+    ws.send('something');
 });
 
 // Default route for webserver
-app.get('/api', function(req, res) {
+app.get('/api', function (req, res) {
     // Try to parse the request object
     messageHandling(req.query['request'], res);
 });
@@ -39,11 +41,11 @@ function messageHandling(requestObjectString, res) {
     let req_obj;
     try {
         req_obj = JSON.parse(requestObjectString);
-    } catch (e){
+    } catch (e) {
         // Catch errors
         res.send({
-            error : 1,
-            error_msg : 'Request object not provided or not in JSON format.'
+            error: 1,
+            error_msg: 'Request object not provided or not in JSON format.'
         });
         return;
     }
@@ -52,8 +54,8 @@ function messageHandling(requestObjectString, res) {
     console.log(method);
     if (!(method in methods)) {
         res.send({
-            error : 2,
-            error_msg : 'No method specified or method not existing.'
+            error: 2,
+            error_msg: 'No method specified or method not existing.'
         });
     } else {
         // Call method
@@ -64,13 +66,13 @@ function messageHandling(requestObjectString, res) {
 // Dictionary containing all methods
 let methods = {
 
-    'get_updates' : (req, res) => {
+    'get_updates': (req, res) => {
         // Getting fridge id
         let fridge_id = req.fridge_id;
         if (fridge_id === undefined || isNaN(fridge_id)) {
             res.send({
-                error : 3,
-                error_msg : 'fridge_id not specified.'
+                error: 3,
+                error_msg: 'fridge_id not specified.'
             });
             return;
         }
@@ -79,19 +81,16 @@ let methods = {
         if (isNaN(state) || state === undefined) {
             state = 0;
         }
-        // TODO: Change to actual database access
-        let response = getUpdates(fridge_id, state);
-        response.error = null;
-        res.send(response);
+        getUpdates(fridge_id, state, (result) => res.send(result));
     },
 
-    'add_update' : (req, res) => {
+    'add_update': (req, res) => {
         // Getting fridge id
         let fridge_id = req.fridge_id;
         if (fridge_id === undefined || isNaN(fridge_id)) {
             res.send({
-                error : 3,
-                error_msg : 'fridge_id not specified.'
+                error: 3,
+                error_msg: 'fridge_id not specified.'
             });
             return;
         }
@@ -99,109 +98,137 @@ let methods = {
         let update = req.update;
         if (update === undefined) {
             res.send({
-                error : 4,
-                error_msg : 'No update blob provided.'
+                error: 4,
+                error_msg: 'No update blob provided.'
             });
             return;
         }
-        let state = addUpdate(fridge_id, update);
-        res.send({
-            new_state : state,
-            error : null
+        console.log(update);
+        addUpdate(fridge_id, update, (state) => {
+            res.send({
+                new_state: state,
+                error: null
+            });
         });
     },
 
-    'barcode_info' : (req, res) => {
+    'barcode_info': (req, res) => {
         // Getting barcodes
         let barcodes = req.barcodes;
         console.log(barcodes);
         if (barcodes === undefined || !Array.isArray(barcodes)) {
             res.send({
-                error : 5,
-                error_msg : 'The barcodes parameter is required and must be an array of barcodes.'
+                error: 5,
+                error_msg: 'The barcodes parameter is required and must be an array of barcodes.'
             });
             return;
         }
         // Build response
         let result = {
-            info : [],
-            error : null
+            info: [],
+            error: null
         }
-        for (var ind in barcodes) {
-            result.info.push({
-                barcode : barcodes[ind],
-                info : getBarcodeInfo(barcodes[ind])
+        for (let ind in barcodes) {
+            getBarcodeInfo(barcodes[ind], (info) => {
+                result.info.push({
+                    barcode : barcodes[ind],
+                    info : info
+                });
+                if (result.info.length == barcodes.length) {
+                    res.send(result);
+                }
             });
         }
-        res.send(result);
     }
 }
 
 // Connect to the Database
-MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to SenneSDB");
+MongoClient.connect(url, {
+    useNewUrlParser: true
+}, function (err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to SenneSDB");
 
-  // Update the dbClient varaiable and use it make connections
-  const dbClient = client.db(dbName);
+    // Update the dbClient varaiable and use it make connections
+    dbClient = client.db(dbName);
 });
 
 
 // This function should return the response for the get_updates method.
-function getUpdates(fridgeId, state) {
+function getUpdates(fridgeId, state, callback) {
 
-  // create the collection object for retrieving documents
-  const collection = dbClient.collection("Fridges");
+    // create the collection object for retrieving documents
+    const collection = dbClient.collection("Fridges");
 
-  // items from the DB are stored in an array of dictionaries
-  // Need to fix this function as it is returning all fridge IDs over the state
-  collection.find( { state: { $gt: currentState }, fridge_id: fridgeID } ).toArray(function(err, items) {
-    if (err) throw err;
+    // items from the DB are stored in an array of dictionaries
+    // Need to fix this function as it is returning all fridge IDs over the state
+    collection.find({
+        state: {
+            $gt: state
+        },
+        fridge_id: fridgeId
+    }).toArray(function (err, items) {
+        if (err) throw err;
 
-    var updates = items.map(i => i.encrpyted_update);
+        var updates = items.map(i => i.encrpyted_update);
 
-    var result = { new_state: Math.max(...items.map(i => i.state)), update: updates, error: null};
-    console.log(result);
+        var result = {
+            new_state: Math.max(...items.map(i => i.state)),
+            update: updates,
+            error: null
+        };
+        console.log(result);
 
-   if (callback != undefined)
-    callback(result);
-  });
+        if (callback != undefined)
+            callback(result);
+    });
 }
 
 // This function saves the update to the database and should return the new state.
 function addUpdate(fridgeId, update, callback) {
 
-  // Create the collection object for inserting documents into Fridges database
-  const collection = dbClient.collection("Fridges");
+    // Create the collection object for inserting documents into Fridges database
+    const collection = dbClient.collection("Fridges");
 
-  // Query the database to find the current highest state for this fridge
-  collection.find( { fridge_id: fridgeID } ).sort({ state:-1}).limit(1).toArray(function(err, maxState) {
-    if (err) throw err;
+    // Query the database to find the current highest state for this fridge
+    collection.find({
+        fridge_id: fridgeId
+    }).sort({
+        state: -1
+    }).limit(1).toArray(function (err, maxState) {
+        if (err) throw err;
 
-    if (maxState.length == 0) {
-     var newState = 1;
-    } else {
-     var newState = maxState[0].state + 1;
-    }
+        if (maxState.length == 0) {
+            var newState = 1;
+        } else {
+            var newState = maxState[0].state + 1;
+        }
 
-    // Create the document containing the ID, state and string
-    let fridgeUpdate = { fridge_id: fridgeID , state: newState, encrpyted_update: update };
+        // Create the document containing the ID, state and string
+        let fridgeUpdate = {
+            fridge_id: fridgeId,
+            state: newState,
+            encrpyted_update: update
+        };
 
-    // insert the document into the DB
-    collection.insertOne(fridgeUpdate, function(err, res) {
-     if (err) throw err;
+        // insert the document into the DB
+        collection.insertOne(fridgeUpdate, function (err, res) {
+            if (err) throw err;
 
-    if (callback != undefined)
-     callback(update);
+            if (callback != undefined)
+                callback(newState);
+        });
     });
-  });
 }
 
 // This function should query the digit-eyes.com database for the given barcode
 function getBarcodeInfo(barcode, callback) {
 
     // Query for UPCitemdb database
-    let query = "https://api.upcitemdb.com/prod/trial/lookup?upc="+barcode;
+    // let query = "https://api.upcitemdb.com/prod/trial/lookup?upc=" + barcode;
+
+    // Query Open Food Facts database
+    let query = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`;
 
     const options = {
         url: query,
@@ -213,22 +240,30 @@ function getBarcodeInfo(barcode, callback) {
     };
 
     // Parse the barcode information
-    request(options, function(err, res, body) {
+    request(options, function (err, res, body) {
+        if (err) throw err;
+        console.log(body);
         let barcodeInfo = JSON.parse(body);
         console.log(barcodeInfo);
-        let items = barcodeInfo.items[0];
+        // let item = barcodeInfo.items[0];
+        let item = barcodeInfo.product;
 
-        // Store the returned JSON object in the Barcodes database
-        const collection = dbClient.collection("Barcodes");
-        collection.insertOne(items, function(err, res) {
-         if (err) throw err;
-
-        var result = { barcode: barcode, info: items, error: "null"}
-        if (callback != undefined)
-         callback(result);
-        });
+        // Check if query has been successful
+        if (barcodeInfo.status == 1) {
+            // Store the returned JSON object in the Barcodes database
+            const collection = dbClient.collection("Barcodes");
+            collection.insertOne(item, function (err, res) {
+                if (err) throw err;
+    
+                if (callback != undefined)
+                    callback(item);
+            });
+        } else {
+            callback({});
+        }
     });
 }
 
-// Start server at port 3000
-app.listen(3000);
+// Start server at port 3001
+app.listen(port);
+console.log(`Opened server at port ${port}!`)
